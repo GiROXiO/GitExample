9765b83e0e42d77daa8a4813a52c86e02131ace5
Jeronimo Antonio Castro Amaya <fbarriosa@uninorte.edu.co>
2025-03-17 08:35:42 -0500

    Feat: Added Person class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import core.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person1 = new Person("Pepito Perez", 20, 1234);
+        System.out.println(person1);
     }
 }
